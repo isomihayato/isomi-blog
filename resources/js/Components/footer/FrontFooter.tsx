838171c473a8d286628e7e6855ffbfbd4bb6086b
isomihayato <isomihayato@gmail.com>
2024-03-28 00:54:02 +0900

    IB-25: フッターのナビゲーション配置を修正。ついでにspフッターとメインコンテンツのマージンを設定。

--- resources/js/Components/footer/FrontFooter.tsx
+++ resources/js/Components/footer/FrontFooter.tsx
@@ -2,34 +2,31 @@ import { Box, Grid } from '@mui/material';
 import React from 'react';
 export default function FrontFooter() {
   const ulStyle = {
-    display: 'flex',
     fontSize: '.9rem',
   };
   return (
     <>
       <div id="footer">
-        <Grid container id="footer__outer">
+        <Grid container id="footer__outer" alignItems="flex-end">
           <Grid item md={3} xs={6}>
             <Box id="logo">INFO BOX</Box>
             <span>情報の玉手箱(成長中)</span>
             <p id="copyright">© 2024 INFO BOX</p>
           </Grid>
           <Grid item md={9} xs={6}>
-            <Grid container alignItems="flex-end" style={{ height: '100%' }}>
-              <Box id="nav">
-                <ul style={ulStyle}>
-                  <li style={{ paddingRight: '10px' }}>
-                    <a href="/">ホーム</a>
-                  </li>
-                  <li>
-                    <a href="/articles/search">記事検索</a>
-                  </li>
-                  <li>
-                    <a href="/infomations/list">お知らせ一覧</a>
-                  </li>
-                </ul>
-              </Box>
-            </Grid>
+            <Box id="nav">
+              <Grid container sx={ulStyle} alignItems="flex-end">
+                <Grid item md={1} xs={12}>
+                  <a href="/">ホーム</a>
+                </Grid>
+                <Grid item md={1} xs={12}>
+                  <a href="/articles/search">記事検索</a>
+                </Grid>
+                <Grid item md={2} xs={12}>
+                  <a href="/infomations/list">お知らせ一覧</a>
+                </Grid>
+              </Grid>
+            </Box>
           </Grid>
         </Grid>
       </div>
