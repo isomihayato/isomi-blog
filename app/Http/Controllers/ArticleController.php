b9bbcf49490cd8e13bd924ba071756f3628127d0
isomihayato <isomihayato@gmail.com>
2024-04-02 22:20:25 +0900

    IB-71: 動的にstorage/public/sitemap.xmlへファイル全体を更新

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
 use App\Models\Article;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
@@ -37,7 +38,7 @@ class ArticleController extends Controller
     public function store(Request $request)
     {
         $now = now();
-        Article::create([
+        $article = Article::create([
             'title' => $request->input('title'),
             'body' => $request->input('body'),
             'describe' => $request->input('describe'),
@@ -47,6 +48,26 @@ class ArticleController extends Controller
             'article_ad_template_id' => $request->input('article_ad_template_id'),
             'published_at' => $request->input('published_at') ?? $now,
         ]);
+       // サイトマップのベース部分を作成
+       $sitemapContent = '<?xml version="1.0" encoding="UTF-8"?>';
+       $sitemapContent .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+       // 固定ページのURLを追加
+       $sitemapContent .= '<url><loc>https://info-space-box.net/</loc><changefreq>monthly</changefreq><priority>0.7</priority></url>';
+       $sitemapContent .= '<url><loc>https://info-space-box.net/articles/search</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>';
+       $sitemapContent .= '<url><loc>https://info-space-box.net/infomations/list</loc><changefreq>monthly</changefreq><priority>0.8</priority></url>';
+
+       // 記事のURLを動的に追加
+       $articles = Article::all(); // すべての記事を取得
+       foreach ($articles as $article) {
+           $sitemapContent .= '<url><loc>https://info-space-box.net/articles/details/' . $article->id . '</loc><changefreq>daily</changefreq><priority>1.0</priority></url>';
+       }
+
+       // サイトマップの終了タグを追加
+       $sitemapContent .= '</urlset>';
+
+       // publicフォルダの中のsitemap.xmlに書き込む
+       Storage::disk('public')->put('sitemap.xml', $sitemapContent);
         return response()->json(['status' => "success"]);
     }
 
